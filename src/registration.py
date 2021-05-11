ba7ab60f553e0b21305416e581b620d940a94012
Kenzananas <bouzidkenza1@gmail.com>
2021-05-11 23:20:15 +0200

    non linear registration

--- src/registration.py
+++ src/registration.py
@@ -1,6 +1,13 @@
 import numpy as np
 import SimpleITK as sitk  
 from utils import *
+from enum import Enum 
+
+class Interpolater(Enum):
+    LINEAR = sitk.sitkLinear
+    NN = sitk.sitkNearestNeighbor
+    SPLINE = sitk.sitkBSpline
+
 
 class Transform():
     def __init__(self, im_ref_filename, im_mov_filename):
@@ -12,30 +19,22 @@ class LinearTransform(Transform):
     def __init__(self, im_ref_filename, im_mov_filename):
         super().__init__(im_ref_filename, im_mov_filename)
 
-    def apply_transf(self, lin_xfm, interp='linear'):
+    def apply_transf(self, lin_xfm, interp=Interpolater.LINEAR):
         """ Apply given linear transform `lin_xfm` to `self.im_mov` and 
         return the transformed image. """
         
         resampler = sitk.ResampleImageFilter()
-
         # Set the reference image
         resampler.SetReferenceImage(self.im_ref)
-
         # Use an interpolator
-        if interp == 'linear':
-            resampler.SetInterpolator(sitk.sitkLinear)
-        elif interp == 'NN':
-            resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-        elif interp == 'spline':
-            resampler.SetInterpolator(sitk.sitkBSpline)
-
+        resampler.SetInterpolator(interp.value)
         # Set the transformation 'lin_xfm'
         resampler.SetTransform(lin_xfm)
         trans_im_mov = resampler.Execute(self.im_mov)
 
         return trans_im_mov
 
-    def est_transf(self, fix_img_mask=None, metric='MI', interp='linear', num_iter=100, gradient_descent_step=1, conv_min_value=1e-6):
+    def est_transf(self, fix_img_mask=None, metric='MI', interp=Interpolater.LINEAR, num_iter=100, gradient_descent_step=1, conv_min_value=1e-6):
         """ Estimate linear transform to align `self.im_mov` to `self.im_ref` and 
         return the transform parameters. """
 
@@ -65,12 +64,7 @@ class LinearTransform(Transform):
             registration_method.SetMetricFixedMask(fix_img_mask)
 
         # interpolator
-        if interp == 'linear':
-            registration_method.SetInterpolator(sitk.sitkLinear)
-        elif interp == 'NN':
-            registration_method.SetInterpolator(sitk.sitkNearestNeighbor)
-        elif interp == 'spline':
-            registration_method.SetInterpolator(sitk.sitkBSpline)
+        registration_method.SetInterpolator(interp.value)
 
         # Gradient descent optimizer
         registration_method.SetOptimizerAsGradientDescent(
@@ -108,12 +102,79 @@ class NonLinearTransform(Transform):
     def __init__(self, im_ref_filename, im_mov_filename):
         super().__init__(im_ref_filename, im_mov_filename)
 
-    def est_lin_transf(self, fix_img, mov_img, fix_img_mask=None, metric='MI', interp='linear', num_iter=100, gradient_descent_step=1, conv_min_value=1e-6):
-        """ Apply given non-linear transform `nl_xfm` to `im_mov` and 
-        return the transformed image."""
-        pass
+    def est_transf(self, fix_img_mask=None, metric='MI', interp=Interpolater.LINEAR, num_iter=100, gradient_descent_step=1, conv_min_value=1e-6, fixed_points=None, moving_points=None):
+        """
+        Estimate non-linear transform to align `im_mov` to `im_ref` and
+        return the transform parameters.
+        """
+        registration_method = sitk.ImageRegistrationMethod()
 
-    def est_transf(self, method='Powell'):
+        # Determine the number of BSpline control points using the physical spacing we want for the control grid.
+        grid_physical_spacing = [30.0, 30.0, 30.0]  # A control point every 30mm
+        image_physical_size = [size*spacing for size,
+                               spacing in zip(self.im_ref.GetSize(), self.im_ref.GetSpacing())]
+        mesh_size = [int(image_size/grid_spacing + 0.5)
+                    for image_size, grid_spacing in zip(image_physical_size, grid_physical_spacing)]
+
+        # Set Initial Transform
+        initial_transform = sitk.BSplineTransformInitializer(image1=self.im_ref,
+                                                            transformDomainMeshSize=mesh_size, order=3)
+        registration_method.SetInitialTransform(initial_transform)
+
+        # Similarity metric settings - choose an appropriate metric to be tested
+        if metric == 'MI':
+            registration_method.SetMetricAsMattesMutualInformation(
+                numberOfHistogramBins=50)
+        elif metric == 'SSD':
+            registration_method.SetMetricAsMeanSquares()
+        elif metric == 'NCC':
+            registration_method.SetMetricAsCorrelation()
+
+        # Settings for metric sampling, usage of a mask is optional. When given a mask the sample points will be
+        # generated inside that region. Also, this implicitly speeds things up as the mask is smaller than the
+        # whole image.
+        registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+        registration_method.SetMetricSamplingPercentage(0.01)
+        if fix_img_mask:
+            registration_method.SetMetricFixedMask(fix_img_mask)
+
+        # Multi-resolution framework.
+        registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
+        registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+        registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+
+        # interpolator
+        registration_method.SetInterpolator(interp.value)
+
+        # Gradient descent optimizer
+        registration_method.SetOptimizerAsGradientDescent(
+            learningRate=gradient_descent_step, numberOfIterations=num_iter, convergenceMinimumValue=conv_min_value, convergenceWindowSize=10)
+        registration_method.SetOptimizerScalesFromPhysicalShift()
+
+        # If corresponding points in the fixed and moving image are given then we display the similarity metric
+        # and the TRE during the registration.
+        if fixed_points and moving_points:
+            registration_method.AddCommand(
+                sitk.sitkStartEvent, metric_and_reference_start_plot)
+            registration_method.AddCommand(
+                sitk.sitkEndEvent, metric_and_reference_end_plot)
+            registration_method.AddCommand(sitk.sitkIterationEvent, lambda: metric_and_reference_plot_values(
+                registration_method, fixed_points, moving_points))
+
+        # perform registration
+        final_transform = registration_method.Execute(
+            sitk.Cast(self.im_ref, sitk.sitkFloat32), sitk.Cast(self.im_mov, sitk.sitkFloat32))
+
+        print(final_transform)
+        print("--------")
+        print("Optimizer stop condition: {0}".format(
+            registration_method.GetOptimizerStopConditionDescription()))
+        print("Number of iterations: {0}".format(
+            registration_method.GetOptimizerIteration()))
+        print("--------")
+        return final_transform
+
+    def apply_transf(self, method='Powell'):
         """ Estimate non-linear transform to align `im_mov` to `im_ref` and 
         return the transform parameters. """
         pass
--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,54 @@ from IPython.display import clear_output
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 
+
 def read_image(file_name):
     img = sitk.ReadImage(file_name)
     img = sitk.Cast(img, sitk.sitkFloat32)
     return img
 
+
+def plot_3d_img_slices(img, cmap="gray"):
+    imageSize = img.GetSize()
+    img_data = sitk.GetArrayFromImage(img)
+    plt.figure(figsize=(12, 8))
+    plt.subplot(131)
+    plt.imshow(img_data[int(imageSize[0]/2), :, :], cmap=cmap)
+    plt.title('Axial')
+    plt.subplot(132)
+    plt.imshow(img_data[:, int(imageSize[1]/2), :], cmap=cmap)
+    plt.title('Coronal')
+    plt.subplot(133)
+    plt.imshow(img_data[:, :, int(imageSize[2]/2)], cmap=cmap)
+    plt.title('Sagittal')
+    plt.show()
+
+
+def plot_3d_img_masked(img, mask, alpha=0.3):
+
+    imageSize = img.GetSize()
+    img_data = sitk.GetArrayFromImage(img)
+    mask_data = sitk.GetArrayFromImage(mask)
+
+    plt.figure(figsize=(12, 8))
+    plt.subplot(131)
+    plt.imshow(img_data[int(imageSize[0]/2), :, :], cmap="gray")
+    plt.imshow(mask_data[int(imageSize[0]/2), :, :],
+               cmap="viridis", alpha=alpha)
+    plt.title('Axial')
+
+    plt.subplot(132)
+    plt.imshow(img_data[:, int(imageSize[1]/2), :], cmap="gray")
+    plt.imshow(mask_data[:, int(imageSize[1]/2), :],
+               cmap="viridis", alpha=alpha)
+    plt.title('Coronal')
+
+    plt.subplot(133)
+    plt.imshow(img_data[:, :, int(imageSize[2]/2)], cmap="gray")
+    plt.imshow(mask_data[:, :, int(imageSize[2]/2)],
+               cmap="viridis", alpha=alpha)
+    plt.title('Sagittal')
+    plt.show()
 
 # Callback invoked by the interact IPython method for scrolling through the image stacks of
 # the two images (moving and fixed).
@@ -87,45 +130,92 @@ def update_multires_iterations():
     multires_iterations.append(len(metric_values))
 
 
-def plot_3d_img_slices(img, cmap="gray"):
-    imageSize = img.GetSize()
-    img_data = sitk.GetArrayFromImage(img)
-    plt.figure(figsize=(12, 8))
-    plt.subplot(131)
-    plt.imshow(img_data[int(imageSize[0]/2), :, :], cmap=cmap)
-    plt.title('Axial')
-    plt.subplot(132)
-    plt.imshow(img_data[:, int(imageSize[1]/2), :], cmap=cmap)
-    plt.title('Coronal')
-    plt.subplot(133)
-    plt.imshow(img_data[:, :, int(imageSize[2]/2)], cmap=cmap)
-    plt.title('Sagittal')
+# Callback we associate with the MultiResolutionIterationEvent, update the
+# index into the metric_values list.
+def metric_update_multires_iterations():
+    global metric_values, multires_iterations
+    multires_iterations.append(len(metric_values))
+
+# Callback we associate with the StartEvent, sets up our new data.
+
+
+def metric_and_reference_start_plot():
+    global metric_values, multires_iterations, reference_mean_values
+    global reference_min_values, reference_max_values
+    global current_iteration_number
+
+    metric_values = []
+    multires_iterations = []
+    reference_mean_values = []
+    reference_min_values = []
+    reference_max_values = []
+    current_iteration_number = -1
+
+# Callback we associate with the EndEvent, do cleanup of data and figure.
+
+
+def metric_and_reference_end_plot():
+    global metric_values, multires_iterations, reference_mean_values
+    global reference_min_values, reference_max_values
+    global current_iteration_number
+
+    del metric_values
+    del multires_iterations
+    del reference_mean_values
+    del reference_min_values
+    del reference_max_values
+    del current_iteration_number
+    # Close figure, we don't want to get a duplicate of the plot latter on.
+    plt.close()
+
+# Callback we associate with the IterationEvent, update our data and display
+# new figure.
+
+
+def metric_and_reference_plot_values(registration_method, fixed_points, moving_points):
+    global metric_values, multires_iterations, reference_mean_values
+    global reference_min_values, reference_max_values
+    global current_iteration_number
+
+    # Some optimizers report an iteration event for function evaluations and not
+    # a complete iteration, we only want to update every iteration.
+    if registration_method.GetOptimizerIteration() == current_iteration_number:
+        return
+
+    current_iteration_number = registration_method.GetOptimizerIteration()
+    metric_values.append(registration_method.GetMetricValue())
+    # Compute and store TRE statistics (mean, min, max).
+    current_transform = sitk.CompositeTransform(
+        registration_method.GetInitialTransform())
+    current_transform.SetParameters(registration_method.GetOptimizerPosition())
+    current_transform.AddTransform(
+        registration_method.GetMovingInitialTransform())
+    current_transform.AddTransform(
+        registration_method.GetFixedInitialTransform().GetInverse())
+    mean_error, _, min_error, max_error, _ = registration_errors(
+        current_transform, fixed_points, moving_points)
+    reference_mean_values.append(mean_error)
+    reference_min_values.append(min_error)
+    reference_max_values.append(max_error)
+
+    # Clear the output area (wait=True, to reduce flickering), and plot current data.
+    clear_output(wait=True)
+    # Plot the similarity metric values.
+    plt.subplot(1, 2, 1)
+    plt.plot(metric_values, 'r')
+    plt.plot(multires_iterations, [metric_values[index]
+             for index in multires_iterations], 'b*')
+    plt.xlabel('Iteration Number', fontsize=12)
+    plt.ylabel('Metric Value', fontsize=12)
+    # Plot the TRE mean value and the [min-max] range.
+    plt.subplot(1, 2, 2)
+    plt.plot(reference_mean_values, color='black', label='mean')
+    plt.fill_between(range(len(reference_mean_values)), reference_min_values, reference_max_values,
+                     facecolor='red', alpha=0.5)
+    plt.xlabel('Iteration Number', fontsize=12)
+    plt.ylabel('TRE [mm]', fontsize=12)
+    plt.legend()
+
+    # Adjust the spacing between subplots so that the axis labels don't overlap.
+    plt.tight_layout()
     plt.show()
-
-
-def plot_3d_img_masked(img, mask, alpha=0.3):
-    
-    imageSize = img.GetSize()
-    img_data = sitk.GetArrayFromImage(img)
-    mask_data = sitk.GetArrayFromImage(mask)
-    
-    plt.figure(figsize=(12, 8))
-    plt.subplot(131)
-    plt.imshow(img_data[int(imageSize[0]/2), :, :], cmap="gray")
-    plt.imshow(mask_data[int(imageSize[0]/2), :, :],
-               cmap="viridis", alpha=alpha)
-    plt.title('Axial')
-
-    plt.subplot(132)
-    plt.imshow(img_data[:, int(imageSize[1]/2), :], cmap="gray")
-    plt.imshow(mask_data[:, int(imageSize[1]/2), :],
-               cmap="viridis", alpha=alpha)
-    plt.title('Coronal')
-    
-    plt.subplot(133)
-    plt.imshow(img_data[:, :, int(imageSize[2]/2)], cmap="gray")
-    plt.imshow(mask_data[:, :, int(imageSize[2]/2)],
-               cmap="viridis", alpha=alpha)
-    plt.title('Sagittal')
-    plt.show()
-
