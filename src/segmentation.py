6a5c0311fadb85d8ef2d04539f2ece21eb313711
Kenzananas <bouzidkenza1@gmail.com>
2021-05-16 00:27:35 +0200

    wip atlas seg

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,55 +1,70 @@
 import numpy as np
 import SimpleITK as sitk
-
-def seg_atlas(im, atlas_ct_list, atlas_seg_list):
-    """ Apply atlas-based segmentation of `im` using the list of CT images in `atlas_ct_list` and
-     the corresponding segmentation masks in `atlas_seg_list`. 
-     Return the resulting segmentation mask after majority voting. """
-    pass
-
+from registration import LinearTransform, NonLinearTransform, Transform
 
 CMN_IMG_PATH = "../data/COMMON_images_masks/common_{0}_image.nii.gz"
 CMN_MASK_PATH = "../data/COMMON_images_masks/common_{0}_mask.nii.gz"
 
 GRP_IMG_PATH = "../data/g3_{0}_image.nii.gz"
-GRP_MASK_PATH = "../datag3_{0}_mask.nii.gz"
+GRP_MASK_PATH = "../data/g3_{0}_mask.nii.gz"
 
 class AtlasSegmentation():
     def __init__(self):
         grp_indices = [59, 60, 61]
         cmn_indices = [40, 41, 42]
         
-        self.cmn_img, self.grp_img = [], []
-        self.cmn_mask, self.grp_mask = [], []
+        self.cmn_img, self.grp_img = {}, {}
+        self.cmn_mask, self.grp_mask = {}, {}
 
-        self.cmn_img_data, self.grp_img_data = [], []
-        self.cmn_mask_data, self.grp_mask_data = [], []
+        self.cmn_img_data, self.grp_img_data = {}, {}
+        self.cmn_mask_data, self.grp_mask_data = {}, {}
 
         for i in range(len(cmn_indices)):
             
-            cmn_img = sitk.ReadImage(
+            self.cmn_img[cmn_indices[i]] = sitk.ReadImage(
                 CMN_IMG_PATH.format(cmn_indices[i]))
-            cmn_img_data = sitk.GetArrayFromImage(cmn_img)
-            self.cmn_img.append(cmn_img)
-            self.cmn_img_data.append(cmn_img_data)
+            self.cmn_img_data[cmn_indices[i]] = sitk.GetArrayFromImage(
+                self.cmn_img[cmn_indices[i]])
 
-            cmn_mask = sitk.ReadImage(
+            self.cmn_mask[cmn_indices[i]] = sitk.ReadImage(
                 CMN_MASK_PATH.format(cmn_indices[i]))
-            cmn_mask_data = sitk.GetArrayFromImage(cmn_mask)
-            self.cmn_mask.append(cmn_mask)
-            self.cmn_mask_data.append(cmn_mask_data)
+            self.cmn_mask_data[cmn_indices[i]] = sitk.GetArrayFromImage(
+                self.cmn_mask[cmn_indices[i]])
 
-            grp_img = sitk.ReadImage(
+            self.grp_img[grp_indices[i]] = sitk.ReadImage(
                 GRP_IMG_PATH.format(grp_indices[i]))
-            grp_img_data = sitk.GetArrayFromImage(grp_img)
-            self.grp_img.append(grp_img)
-            self.grp_img_data.append(grp_img_data)
+            self.grp_img_data[grp_indices[i]] = sitk.GetArrayFromImage(
+                self.grp_img[grp_indices[i]])
 
-            grp_mask = sitk.ReadImage(
+            self.grp_mask[grp_indices[i]] = sitk.ReadImage(
                 GRP_MASK_PATH.format(grp_indices[i]))
-            grp_mask_data = sitk.GetArrayFromImage(grp_mask)
-            self.grp_mask.append(grp_mask)
-            self.grp_mask_data.append(grp_mask_data)
+            self.grp_mask_data[grp_indices[i]] = sitk.GetArrayFromImage(
+                self.grp_mask[grp_indices[i]])
 
     def majority_voting(self, reg_masks):
+        """Provide the majority voting result for a list of segmented images. The images must be nd-arrays"""
         pass
+
+    def seg_atlas(self, id_cmn):
+        """ Apply atlas-based segmentation of `im` using the list of CT images in `atlas_ct_list` and
+        the corresponding segmentation masks in `atlas_seg_list`. 
+        Return the resulting segmentation mask after majority voting. """
+        reg_mask = []
+        for id_grp in self.grp_img:
+            lin_trf = LinearTransform(
+                im_ref=self.cmn_img[id_cmn], im_mov=self.grp_img[id_grp])
+            lin_xfm = lin_trf.est_transf(metric="MI", num_iter=200)
+            
+            lin_reg_img = lin_trf.apply_transf(lin_xfm)
+            lin_reg_mask = lin_trf.apply_transf(transformation=lin_xfm, im=self.grp_mask[id_grp])
+
+            nl_trf = NonLinearTransform(
+                im_ref=self.cmn_img[id_cmn], im_mov=lin_reg_img)
+            nl_xfm = nl_trf.est_transf(metric="SSD", num_iter=200)
+
+            nl_reg_mask = nl_trf.apply_transf(
+                transformation=nl_xfm, im=lin_reg_mask)
+
+            reg_mask.append(nl_reg_mask)
+
+        return self.majority_voting(reg_mask)
