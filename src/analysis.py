50348c03964dccdec517f15321d0548ee33f577e
Kenzananas <bouzidkenza1@gmail.com>
2021-05-28 14:42:06 +0200

    crop images

--- src/analysis.py
+++ src/analysis.py
@@ -14,29 +14,30 @@ LABELS = {59: {"size": 275, "pos": (63, 89)},
           61: {"size": 339, "pos": (97, 138)}}
 N_IMG = 276 + 242 + 340
 
+
 class PelvicData():
     def __init__(self, root_path="../", split=0.05):
-        
+
         grp_indices = [59, 60, 61]
         cmn_indices = [40, 41, 42]
 
-        ## Create Labels
+        # Create Labels
         i = 0
         self.y_train = np.zeros(N_IMG)
         for _, v in LABELS.items():
             self.y_train[i+v["pos"][0]:i+v["pos"][1]+1] = 1
             i += v['size']
 
-        ## Create Features
+        # Create Features
         self.grp_img = {}
         self.cmn_img = {}
         self.cmn_img_3c = {}
 
         for i in range(len(cmn_indices)):
             grp_img = sitk.ReadImage(
-                root_path + GRP_IMG_PATH.format(grp_indices[i]))
+                root_path + GRP_IMG_PATH.format(grp_indices[i]))[:, 100:400, 100:400]
             cmn_img = sitk.ReadImage(
-                root_path + CMN_IMG_PATH.format(cmn_indices[i]))
+                root_path + CMN_IMG_PATH.format(cmn_indices[i]))[:, 100:400, 100:400]
 
             self.grp_img[grp_indices[i]] = sitk.GetArrayFromImage(grp_img)
             self.cmn_img[cmn_indices[i]] = sitk.GetArrayFromImage(cmn_img)
@@ -44,8 +45,8 @@ class PelvicData():
                 [np.repeat(x[None, ...], 3, axis=0).T for x in self.cmn_img[cmn_indices[i]]])
 
         self.X_train = np.concatenate(list(self.grp_img.values()), axis=0)
-        
-        ## Preprocess data (scale 0-1)
+
+        # Preprocess data (scale 0-1)
         self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(
             self.X_train, self.y_train, test_size=split, random_state=42, stratify=self.y_train)
 
@@ -64,11 +65,6 @@ class PelvicData():
             [np.repeat(x[None, ...], 3, axis=0).T for x in self.X_train])
         self.X_val_3C = np.array(
             [np.repeat(x[None, ...], 3, axis=0).T for x in self.X_val])
-
-
-
-            
-            
 
 
 def train_classifier(im_list, labels_list):
@@ -97,7 +93,7 @@ def dice_analysis(mask, ref_mask):
     overlap_measures_filter.Execute(ref_mask, mask)
     result = overlap_measures_filter.GetDiceCoefficient()
 
-    #print(result)
+    # print(result)
     return result
 
 
@@ -113,5 +109,5 @@ def hausdorf_distance_analysis(mask, ref_mask):
 
     hausdorff_distance_filter.Execute(ref_mask, mask)
     result = hausdorff_distance_filter.GetHausdorffDistance()
-    #print(result)
+    # print(result)
     return result
